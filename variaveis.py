54fc120b220e186b3d74b386f2a7d2395982fc86
Lucas Corrêa <63137745+LukeCreater@users.noreply.github.com>
2021-01-28 19:36:24 -0300

    Variáveis em Python
    String, Int, Float e Bool

---
+++ variaveis.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+texto = "Teste de variaveis" #String
+numero = 1 #Int
+flutuante = 1.1 #Float
+python = True #Bool
+print(texto)
+print(numero)
+print(flutuante)
+print(python)
