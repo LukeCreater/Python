cd19bf195a0220a44fd34e7b977854ba2217ddd7
Lucas Corrêa <63137745+LukeCreater@users.noreply.github.com>
2021-01-28 20:16:05 -0300

    Operadores em Python

---
+++ Operadores.py
@@ -0,0 +1,10 @@
+x = 3
+y = 2
+print (x == y) #Espera-se False pois X é maior que Y;
+print (x > y) #Espera-se True pois X é maior;
+print (x < y) #Espera-se False pois Y é menor;
+soma = x + y #Espera-se somar X + Y para dar 5;
+print (soma) #Espera-se 5, pois é o resultado de X + Y;
+print (soma > y) #Espera-se True, pois é maior;
+print (soma <= y) #Espera-se False, pois não é menor e nem igual;
+print (soma >= y) #Espera-se True, pois é maior mas não é igual.
