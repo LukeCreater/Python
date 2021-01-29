1d3262c34260d813f57f541af087992f3951c381
Lucas Corrêa <63137745+LukeCreater@users.noreply.github.com>
2021-01-29 14:38:28 -0300

    Manipulando Strings (Parte 2)

---
+++ Manipulando_Strings2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+brasil = "Olá pessoas do meu Brasil"
+busca = brasil.find("pessoas")
+print (busca)
+print (brasil[busca:])
+brasil = brasil.replace("Brasil", "país")
+print(brasil)
+pessoa = "Olá minha gente"
+variavel = pessoa.split(" ")
+print (variavel)
