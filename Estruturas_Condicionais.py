a8624cc9ee8770015f332ce3f23971f8db282a57
Lucas Corrêa <63137745+LukeCreater@users.noreply.github.com>
2021-01-28 21:42:46 -0300

    if, else e elif

--- Estruturas_Condicionais.py
+++ Estruturas_Condicionais.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
-x = 10
-y = 20
+x = 3
+y = 2
 if x > y:
 	print("X é maior que Y")
+elif x < y:
+	print("X é menor que Y")
+elif x == y:
+	print("X é igual a Y")
 else:
 	print("X não é maior que Y")
