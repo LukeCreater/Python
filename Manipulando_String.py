4ad157859f50061875f628e82ce6dbc94530ed4f
Lucas Corrêa <63137745+LukeCreater@users.noreply.github.com>
2021-01-29 14:22:02 -0300

    Manipulação de String (Parte 1)

---
+++ Manipulando_String.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+a = "Olá pessoas do "
+b = "Brasil"
+juntar = a + "meu " + b
+print (juntar)
+tamanho = len(juntar)
+print (tamanho)
+print (a[1])
+print (a[0:3])
+normal = "Olá mundo"
+print (normal.lower())
+print (normal.upper())
+normal = normal + "\n" + "tudo bem?"
+print (normal)
+variavel = "oi" + "\n"
+print (variavel.strip())
