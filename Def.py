65b2a01d159d998384db9ed5d860e7da36f4c1ca
Lucas Corrêa <63137745+LukeCreater@users.noreply.github.com>
2021-01-29 15:30:55 -0300

    Utilizando Def

---
+++ Def.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+def soma (x, y):
+	return x + y
+
+s = soma(2, 3)
+
+def multiplicacao(x, y):
+	return x * y
+
+m = multiplicacao(2, 2)
+
+print(s)
+print(m)
+
+print(soma(s, m))
+print(multiplicacao(s, m))
